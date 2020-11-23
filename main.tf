36f4235883791bd0ae0c5d4e7349b95a5e9ff6d7
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-23 20:36:23 +0530

    pipeline and Vnet

--- main.tf
+++ main.tf
@@ -30,3 +30,21 @@ resource "azurerm_resource_group" "rg-tcloud-azure" {
   name     = "rg-tcloud-azure"
   location = "northcentralus"
 }
+
+resource "azurerm_virtual_network" "azcloud" {
+  name                = "az-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-tcloud-azure.location
+  resource_group_name = azurerm_resource_group.rg-tcloud-azure.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg-tcloud-azure.name
+  virtual_network_name = azurerm_virtual_network.tfcloud.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints = ["Microsoft.Storage"]
+}
+
+
+
