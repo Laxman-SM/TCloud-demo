ace140cb553ee2f9747502d9a2ce9aa89634c4b1
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-23 17:51:47 +0530

    storage account name update

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "azurerm_subnet" "internal" {
   service_endpoints = ["Microsoft.Storage"]
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "tfstorage" {
   name                     = "tfendpint"
   location                 = azurerm_resource_group.rg-tcloud-azure.location
   resource_group_name      = azurerm_resource_group.rg-tcloud-azure.name
