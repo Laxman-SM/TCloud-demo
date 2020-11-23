963e180bd5ead29ed18e158344b0417f3925a802
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-23 17:22:24 +0530

    Vnet storage account configuration added

--- main.tf
+++ main.tf
@@ -31,3 +31,33 @@ resource "azurerm_resource_group" "rg-tcloud-azure" {
   location = "northcentralus"
 }
 
+resource "azurerm_virtual_network" "tfcloud" {
+  name                = "tf-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-tcloud-azure.location
+  resource_group_name = azurerm_resource_group.rg-tcloud-azure.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg-tcloud-azure.name
+  virtual_network_name = azurerm_virtual_network.rg-tcloud-azure.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints = ["Microsoft.Storage"]
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "tfendpint"
+  location                 = azurerm_resource_group.rg-tcloud-azure.location
+  resource_group_name      = azurerm_resource_group.rg-tcloud-azure.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+    network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = [azurerm_subnet.internal.id]
+  }
+}
