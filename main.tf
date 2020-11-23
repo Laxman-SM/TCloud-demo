4032bfaa5263074781f844e92bc1146fbff63142
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-23 17:44:26 +0530

    Misconfigured name updated

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_virtual_network" "tfcloud" {
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg-tcloud-azure.name
-  virtual_network_name = azurerm_virtual_network.tcloud-network.name
+  virtual_network_name = azurerm_virtual_network.tfcloud.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints = ["Microsoft.Storage"]
 }
