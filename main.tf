1a49a9782ce09aa1e3f62a7dd2c67c4b076961f8
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-23 18:39:53 +0530

    minimum tls parameter added

--- main.tf
+++ main.tf
@@ -52,7 +52,16 @@ resource "azurerm_storage_account" "tfstorage" {
   resource_group_name      = azurerm_resource_group.rg-tcloud-azure.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
+  min_tls_version      = "TLS1_2"
+  queue_properties  {
+  logging {
+        delete                = true
+        read                  = true
+        write                 = true
+        version               = "1.0"
+        retention_policy_days = 10
+    }
+  }
   tags = {
     environment = "staging"
   }
