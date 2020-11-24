e7dd2e1d6ec9e27ca219011430ecc44fae3ac1ef
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-25 00:37:32 +0530

    replace variables with files

--- main.tf
+++ main.tf
@@ -55,14 +55,6 @@ resource "azurerm_subnet" "internal" {
 #  version = "4.0.0"
 #}
 
-variable "environment" {
-  default = "development"
-  }
-
-variable "location" {
-  default = "northcentralus"
-  }
-
 
 resource "azurerm_resource_group" "rg" {
   name     = "tf-var.environment-rg"
--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "environment" {
   description = "Name of the environment"
+  default = "development"
 }
 
 variable "location" {
   description = "Azure location to use"
+  location = "northcentralus"
 }
 
