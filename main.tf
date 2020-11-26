58b4c3dff901961b209faedd4cc84a8437914e72
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-27 01:11:09 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.37.0"
-    }
-    random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/azurerm"
+      version = "~> 2.33.0"
     }
   }
 
@@ -20,27 +18,219 @@ terraform {
 }
 
 provider "azurerm" {
-  # skip provider rego because we are using a service principal with limited access to Azure
-  skip_provider_registration = "true"
+/*   
+  skip_provider_registration = true
+  tenant_id       = "your tenant id"
+  subscription_id = "your subscription id"
+  client_id       = "your service principal appId"
+  client_secret   = "your service principal password" 
+*/
   features {}
 }
 
-resource "azurerm_resource_group" "rg-tcloud-azure" {
-  name     = "rg-tcloud-azure"
-  location = "northcentralus"
+module "policy_assignments" {
+  source = "./modules/policy-assignments"
+
+  monitoring_governance_policyset_id      = module.policyset_definitions.monitoring_governance_policyset_id
+  tag_governance_policyset_id             = module.policyset_definitions.tag_governance_policyset_id
+  iam_governance_policyset_id             = module.policyset_definitions.iam_governance_policyset_id
+  security_governance_policyset_id        = module.policyset_definitions.security_governance_policyset_id
+  data_protection_governance_policyset_id = module.policyset_definitions.data_protection_governance_policyset_id
 }
 
-resource "azurerm_virtual_network" "azcloud" {
-  name                = "az-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg-tcloud-azure.location
-  resource_group_name = azurerm_resource_group.rg-tcloud-azure.name
+module "policy_definitions" {
+  source = "./modules/policy-definitions"
+
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg-tcloud-azure.name
-  virtual_network_name = azurerm_virtual_network.azcloud.name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints = ["Microsoft.Storage"]
+module "policyset_definitions" {
+  source = "./modules/policyset-definitions"
+
+  custom_policies_monitoring_governance = [
+    {
+      policyID = module.policy_definitions.expressRouteGateway_bitsOutPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteGateway_bitsInPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuitPeer_bitsOutPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuitPeer_bitsInPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuit_bitsOutPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuit_bitsInPerSecond_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuit_bgpAvailability_policy_id
+    },
+    {
+      policyID = module.policy_definitions.expressRouteCircuit_arpAvailability_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlServerDB_storagePercent_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlServerDB_deadlock_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlServerDB_cpuPercent_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlServerDB_connectionFailed_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlServerDB_blockedByFirewall_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlManagedInstances_ioRequests_policy_id
+    },
+    {
+      policyID = module.policy_definitions.sqlManagedInstances_avgCPUPercent_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_failedRequests_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_healthyHostCount_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_unhealthyHostCount_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_totalRequests_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_cpuUtilization_policy_id
+    },
+    {
+      policyID = module.policy_definitions.appGateway_clientRTT_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websvrfarm_cpuPercentage_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websvrfarm_memoryPercentage_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_averageMemoryWorkingSet_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_averageResponseTime_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_cpuTime_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_healthCheckStatus_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_http5xx_policy_id
+    },
+    {
+      policyID = module.policy_definitions.website_requestsInApplicationQueue_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_averageMemoryWorkingSet_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_averageResponseTime_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_cpuTime_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_healthCheckStatus_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_http5xx_policy_id
+    },
+    {
+      policyID = module.policy_definitions.websiteSlot_requestsInApplicationQueue_policy_id
+    },
+    {
+      policyID = module.policy_definitions.azureFirewall_health_policy_id
+    },
+    {
+      policyID = module.policy_definitions.loadBalancer_dipAvailability_policy_id
+    },
+    {
+      policyID = module.policy_definitions.loadBalancer_vipAvailability_policy_id
+    }
+  ]
+
+  custom_policies_tag_governance = [
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[0]
+    },
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[1]
+    },
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[2]
+    },
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[3]
+    },
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[4]
+    },
+    {
+      policyID = module.policy_definitions.addTagToRG_policy_ids[5]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[0]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[1]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[2]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[3]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[4]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[5]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[0]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[1]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[2]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[3]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[4]
+    },
+    {
+      policyID = module.policy_definitions.inheritTagFromRGOverwriteExisting_policy_ids[5]
+    },
+    {
+      policyID = module.policy_definitions.bulkInheritTagsFromRG_policy_id
+    }
+  ]
+
+  custom_policies_iam_governance = [
+    {
+      policyID = module.policy_definitions.auditRoleAssignmentType_user_policy_id
+    },
+    {
+      policyID = module.policy_definitions.auditLockOnNetworking_policy_id
+    }
+  ]
 }
+
+
