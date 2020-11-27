a441f569fe6aa9f9f54328afced4209470469379
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-27 16:23:17 +0530

    removed temporary directory

--- temp/acr.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr.name
-  resource_group_name = var.resource_group
-  location            = var.location
-  sku                 = var.acr.sku
-  admin_enabled       = var.acr.admin_enabled
-
-  tags = var.tags
-}
